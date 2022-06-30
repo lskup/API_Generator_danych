efc5f4dac3532f6e6aeb4da575617f4a4d9d65b5
lskup <lskup23@gmail.pl>
2022-07-01 01:49:41 +0200

    1.1 Logging Fix

--- API_Generator_danych/Program.cs
+++ API_Generator_danych/Program.cs
@@ -10,31 +10,42 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(((ctx, lc) => lc
             .ReadFrom.Configuration(ctx.Configuration)));
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ISqlAccess, SqlAccess>();
-builder.Services.AddSingleton<IUserData, UserData>();
-builder.Services.AddSingleton<Random>();
-builder.Services.AddSingleton<IPersonsGenerator, PersonsGenerator>();
-builder.Services.AddSingleton<ICustomerData, CustomerData>();
-builder.Services.AddHttpContextAccessor();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.UseSerilogRequestLogging();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddSingleton<ISqlAccess, SqlAccess>();
+    builder.Services.AddSingleton<IUserData, UserData>();
+    builder.Services.AddSingleton<Random>();
+    builder.Services.AddSingleton<IPersonsGenerator, PersonsGenerator>();
+    builder.Services.AddSingleton<ICustomerData, CustomerData>();
+    builder.Services.AddHttpContextAccessor();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.ConfigureEndpoints();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.ConfigureEndpoints();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
